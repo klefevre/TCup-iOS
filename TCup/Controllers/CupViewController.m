43d243bc89e043ca765a1e7fc4bf61d06ceb1615
Kevin Lefevre <kevin.lefevre@in-tact.fr>
2015-09-12 23:33:09 +0200

    Add tea cup screen

---
+++ TCup/Controllers/CupViewController.m
@@ -0,0 +1,32 @@
+//
+//  CupViewController.m
+//  TCup
+//
+//  Created by Kevin Lefevre on 12/09/2015.
+//  Copyright (c) 2015 BeMyApp. All rights reserved.
+//
+
+#import "CupViewController.h"
+
+@interface CupViewController ()
+
+@property (nonatomic, weak) IBOutlet UIProgressView *progressView;
+
+@end
+
+@implementation CupViewController
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark -
+
+@end
