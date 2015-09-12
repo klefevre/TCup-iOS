561f1e552943377bb425506286e35d326853224a
Kevin Lefevre <kevin.lefevre@in-tact.fr>
2015-09-12 18:21:28 +0200

    Implement BTService

--- TCup/ViewController.m
+++ TCup/ViewController.m
@@ -6,22 +6,56 @@
 //  Copyright (c) 2015 BeMyApp. All rights reserved.
 //
 
+#import <CoreBluetooth/CoreBluetooth.h>
+
 #import "ViewController.h"
 
-@interface ViewController ()
+#import "BTDiscovery.h"
+
+@interface ViewController () <BTDiscoveryDelegate>
+
+@property (nonatomic, strong) BTDiscovery *btDiscovery;
 
 @end
 
 @implementation ViewController
 
+#pragma mark - View lifecycle
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    self.btDiscovery = [[BTDiscovery alloc] initWithDelegate:self];
+
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Not connected"
+                                                                              style:UIBarButtonItemStylePlain
+                                                                             target:nil
+                                                                             action:nil];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Actions
+
+- (IBAction)connect:(id)sender {
+//    [self.btDiscovery startScanning];
+}
+
+#pragma mark - BTDiscoveryDelegate
+
+- (void)peripheralConnecting {
+    self.navigationItem.rightBarButtonItem.title = @"Connecting";
+}
+
+- (void)peripheralDidConnect {
+    self.navigationItem.rightBarButtonItem.title = @"Connected";
+}
+
+- (void)peripheralDidDisconnect {
+    self.navigationItem.rightBarButtonItem.title = @"Not connected";
 }
 
 @end
